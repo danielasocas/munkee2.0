c8d4d8620c7475eb828333d9bd304ffe489089f2
danielasocas <danielasocasgiil@gmail.com>
2016-12-11 05:44:00 +0000

    some login actions added, needs testing

---
+++ actions/action_login.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+include_once('../db/connection.php');
+include_once('../db/users.php');
+
+if (userExists($db, $_POST['username'], $_POST['password'])){
+    $_SESSION['username'] = $_POST['username'];
+    $_SESSION['user-logged'] = true;
+}
+
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+
+?>
